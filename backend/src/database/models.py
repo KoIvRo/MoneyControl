a176ee844861b9634d328275c60f5b0e2928e910
unknown <korot4ev29@gmail.com>
2025-11-22 22:08:51 +1000

    feat: База данных и модели

---
+++ backend/src/database/models.py
@@ -0,0 +1,41 @@
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Column, Integer, String, DateTime, Numeric, ForeignKey
+
+
+class Base(DeclarativeBase):
+    """Класс для ORM базы данных."""
+
+    pass
+
+
+class User(Base):
+    """Модель аккаунта пользователя в базе данных."""
+
+    __tablename__ = "users"
+    id = Column(Integer(), primary_key=True, autoincrement=True, index=True)
+    name = Column(String(20))
+    email = Column(String(30))
+    password = Column(String(128))
+
+
+class Account(Base):
+    """Банковские счета пользователя в базе данных."""
+
+    __tablename__ = "accounts"
+    id = Column(Integer(), primary_key=True, autoincrement=True, index=True)
+    name = Column(String(20))
+    balance = Column(Integer())
+    user_id = Column(Integer(), ForeignKey("users.id"))
+
+
+class Transaction(Base):
+    """Транзакции пользователя привязанные к счету."""
+
+    __tablename__ = "transaction"
+    id = Column(Integer(), primary_key=True, autoincrement=True, index=True)
+    amount = Column(Numeric())
+    category = Column(String(20))
+    date = Column(DateTime())
+    comment = Column(String(100))
+    account_id = Column(Integer(), ForeignKey("accounts.id"))
+    user_id = Column(Integer(), ForeignKey("users.id"))
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,20 @@
-from fastapi import FastAPI
 import uvicorn
+from fastapi import FastAPI
+from database.database import create_db
 
 app = FastAPI()
 
 
 def main() -> None:
+    """Точка запуска REST API."""
+    create_db()
     uvicorn.run("main:app", reload=True, host="0.0.0.0", port=8000)
 
 
 @app.get("/")
-def root_get() -> None:
+async def root_get() -> None:
     return {"title": "Hello, World!"}
+
+
+if __name__ == "__main__":
+    main()
